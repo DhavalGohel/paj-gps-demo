37481a80d504356953f006e25dd9f6ea0897b730
Dhaval Gohel <goheldhaval20@gmail.com>
2024-08-20 01:04:30 +0530

    intial commit

--- src/main.ts
+++ src/main.ts
@@ -4,33 +4,40 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
-import { provideHttpClient, withInterceptors, HttpInterceptorFn, HttpErrorResponse } from '@angular/common/http';
+import { provideHttpClient, withInterceptors, HttpInterceptorFn, HttpErrorResponse, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
 import { TOKEN_KEY } from './constants/app.constant';
-import { catchError, throwError } from 'rxjs';
+import { catchError, from, Observable, switchMap, throwError } from 'rxjs';
 import { inject } from '@angular/core';
 import { AuthenticationService } from './app/services/authentication.service';
+import { Storage } from '@capacitor/storage';
 
 const authInterceptorServiceInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(AuthenticationService)
   const router = inject(Router)
-  const token = localStorage.getItem(`CapacitorStorage.${TOKEN_KEY}`);
-  if (token) {
-    req = req.clone({
-      setHeaders: {
-        Authorization: `Bearer ${token}`
+  return from(Storage.get({ key: TOKEN_KEY })).pipe(
+    switchMap(({ value: token }) => {
+      // Clone the request to add the new header
+      if (token) {
+        req = req.clone({
+          setHeaders: {
+            Authorization: `Bearer ${token}`
+          }
+        });
       }
-    });
-  }
-  return next(req).pipe(
-    catchError((error: HttpErrorResponse) => {
-      if (error.status === 401) { // Unauthorized
-        authService.logout(); // Handle logout
-        router.navigateByUrl('/login', { replaceUrl: true }); // Redirect to login page
-      }
-      return throwError(error);
+
+      // Pass the cloned request instead of the original request to the next handler
+      return next(req).pipe(
+        catchError((error: HttpErrorResponse) => {
+          if (error.status === 401) { // Unauthorized
+            authService.logout(); // Handle logout
+            router.navigateByUrl('/login', { replaceUrl: true }); // Redirect to login page
+          }
+          return throwError(error);
+        })
+      );
     })
   );
-}
+};
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
